5f3f3284e225073f7c25dd38d9f51f5331354286
ajiaron <aaronjiang2001@gmail.com>
2023-08-19 21:04:24 -0700

    configred fastAPI

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
-import streamlit as st
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # silence tensorflow warnings
+
 import tensorflow as tf
 import yfinance as yf
 from pyESN import ESN
 import matplotlib.pyplot as plt
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class GrabDataInput(BaseModel):
+    symbol: str
+    start_date: str
+    end_date: str
+    intervals: str
+
+@app.post("/grab_data")
+def grab_data_endpoint(data: GrabDataInput):
+    result = grab_data(data.symbol, data.start_date, data.end_date, data.intervals)
+    return {'result': result}
+
 def grab_data(symbol, start_date, end_date, intervals):
     stock_data = yf.download(symbol, start=start_date, end=end_date, interval=intervals ,progress=False)
     data = stock_data["Close"].values
-    return data
+    if not len(data):
+        raise HTTPException(status_code=404, detail="Data not found")
+    if isinstance(data, pd.DataFrame):
+        return data.to_dict()
+    elif isinstance(data, np.ndarray):
+        return data.tolist()
+    else:
+        raise HTTPException(status_code=500, detail="Unexpected data type")
+
     
+@app.get("/MSE")
 def MSE(prediction, actual):
     return np.mean(np.power(np.subtract(np.array(prediction),actual),2))
 
-
+@app.get("/run_echo")
 def run_echo(data, reservoir_size=500, sr=1.2, n=0.005, window=5):
     esn = ESN(n_inputs = 1,
           n_outputs = 1,
@@ -34,9 +62,9 @@ def run_echo(data, reservoir_size=500, sr=1.2, n=0.005, window=5):
         current_set.append(prediction[0])
     current_set = np.reshape(np.array(current_set),(-1,100))
     mse = MSE(current_set, data[trainlen:trainlen+100])
-    
     return (mse, current_set)
 
+@app.post('/future_pred')
 def future_pred(data, reservoir_size=500, sr=1.2, n=0.005, window=5):
     esn = ESN(n_inputs = 1,
           n_outputs = 1,
@@ -48,8 +76,18 @@ def future_pred(data, reservoir_size=500, sr=1.2, n=0.005, window=5):
     pred_training = esn.fit(np.ones(100),data[-100:])
     prediction = esn.predict(np.ones(window))
     return prediction.reshape(-1)
-    
+
+d
+@app.get("/compute/")
+def compute_endpoint(x: int, y: int):
+    return {"result": compute_something(x, y)}
+
+def compute_something(x: int, y: int) -> int:
+    return x + y
+
 def main():
+    print('sup')
+    '''
     st.title("Stock Price Prediction App")
 
     st.write("Enter the stock symbol and dates for prediction:")
@@ -69,6 +107,7 @@ def main():
         prediction = future_pred(data, 500, 1.2, .005, 5)
         st.write(prediction)
         # You can call your prediction function and display the results here
+'''
 
 if __name__ == "__main__":
     main()
