fc58c0c44139155fa160c2e1c8c6de02727b4962
Alex Ngo <42555152+alngo1@users.noreply.github.com>
2023-09-11 14:10:51 -0700

    dropdown/react testing

---
+++ stockbot/src/Testing.js
@@ -0,0 +1,19 @@
+import './Testing.scss'
+import React, {useState} from 'react'
+import './Testing.scss'
+export default function Testing() {
+  const [shouldDropdown, setShouldDropdown] = useState(false)
+  return (
+    <div style={{color:"white"}}>
+        Testing
+        <span onClick={()=>setShouldDropdown(!shouldDropdown)}>
+          click me
+        </span>
+        {(shouldDropdown)&&
+            <div className='dropdown'>
+              stuff
+            </div>
+        }
+    </div>
+  )
+}
